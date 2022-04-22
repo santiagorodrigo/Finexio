35f8b00ce1e0c627b7ef36054791f3c9bae646bd
rodrigosantiago <santiago.ti@gmail.com>
2022-04-22 11:23:26 -0500

    Question_2

---
+++ Finexio_UTests/Question_1.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace Finexio_UTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ Finexio_UTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace Finexio_UTests;
+
+public class Question_1
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        // Arrange
+        Finexio
+        Assert.Pass();
+    }
+}
