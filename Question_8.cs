f9ba685c9602c5b5e57cb85af04d7d6009c79006
rodrigosantiago <santiago.ti@gmail.com>
2022-04-22 13:15:43 -0500

    Question 8

---
+++ Question_8.cs
@@ -0,0 +1,26 @@
+﻿//8.Implement the following method using reflection.The method should locate a public property on instance with the name passed in via the propertyName parameter, and set the property's value to propertyValue.    (10 points)
+//void SetPropertyOnType<T>(T instance, string propertyName, string propertyValue)
+//{
+
+//}
+
+
+using System;
+using System.Reflection;
+
+namespace Finexio
+{
+	public class Question_8
+	{
+		void SetPropertyOnType<T>(T instance, string propertyName, string propertyValue)
+		{
+			PropertyInfo pointerToProperty = typeof(T).GetProperty(propertyName);
+			if (pointerToProperty != null)
+			{				
+				pointerToProperty.SetValue(instance, propertyValue, null);
+			}			
+
+		}
+	}
+}
+
