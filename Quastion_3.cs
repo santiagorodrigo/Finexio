fa1dd4377500f9f3c60a2b381ab005556cf20562
rodrigosantiago <santiago.ti@gmail.com>
2022-04-22 11:47:57 -0500

    Question 3

---
+++ Quastion_3.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+//3.Which is the correct approach to using an object that implements IDisposable in C#: (10 points)
+//1.
+//void DoSomething()
+//{
+//    var connection = new SqlConnection(ConnectionString);
+
+//    // Use connection
+//}
+
+//2.
+//void DoSomething()
+//{
+//    var connection = new SqlConnection(ConnectionString);
+
+//    // Use connection
+
+//    connection.Dispose();
+//}
+
+//3.
+//void DoSomething()
+//{
+//    using (var connection = new SqlConnection(ConnectionString))
+//    {
+//        // Use connection
+//    }
+//}
+
+
+namespace Finexio
+{
+    public void DoSomething()
+    {
+        using (var connection = new SqlConnection(ConnectionString))
+        {
+            // Use connection
+            // This solution is better because all objects created inside the
+            // using block will be disposeddisposal by its end.
+        }
+    }
+}
+
